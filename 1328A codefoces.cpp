04b575280f297785d43d8f0a9735edf12e5488f4
Mehedi Hasan Rafi <136667687+mhrafi39@users.noreply.github.com>
2023-10-08 19:45:46 +0600

    Divisibility Problem
    problem link:- https://codeforces.com/contest/1328/problem/A

---
+++ 1328A codefoces.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t,a,b;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>a>>b;
+        if(a%b==0)
+            cout<<"0\n";
+        else
+            cout<<b-(a%b)<<endl;
+ 
+ 
+    }
+}
